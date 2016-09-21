ac5761267b33fd20158d50f26ffe78204b119776
Nathan Miller <nathanm2@gmail.com>
2016-09-20 23:35:00 -0500

    More progress on cscope_db

--- cscope_db/cscope_db.py
+++ cscope_db/cscope_db.py
@@ -6,17 +6,46 @@ files.
 """
 
 import argparse
+import sys
+import os
+import inspect
 
-cmd_list=("join", "list-cfgs", "cfg-path")
+def script_dir():
+    """ Get the path of THIS script."""
+    return os.path.dirname(os.path.abspath(inspect.getfile(
+        inspect.currentframe())))
+
+CONFIGS_DIR=os.path.join(script_dir(), "configs")
+
+def get_configs():
+    return {cfg: os.path.join(CONFIGS_DIR,cfg) for cfg in
+            os.listdir(CONFIGS_DIR) if not cfg.startswith('.')}
+
 
 if __name__ == "__main__":
 
+    def error(msg):
+        sys.stderr.write("** {}: {}\n".format(__file__, msg))
+        sys.exit(1)
+
+    cmd_list=("list-configs", "config-path", "attach")
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("cmd",
-                        help="The operation to perform",
+    parser.add_argument("cmd", nargs='?',
+                        help="command to execute",
                         choices=cmd_list,
-                        default="join")
+                        default="attach")
     parser.add_argument("-c", "--config",
-                        help="Specify the configuration to use.",
+                        help="cscope configuration",
                         default="basic")
     args = parser.parse_args()
+
+    # Run the specified command:
+    if args.cmd == "list-configs":
+        for key in get_configs().keys():
+            print "  {}".format(key)
+    elif args.cmd == 'config-path':
+        path = get_configs().get(args.config)
+        if path:
+            print path
+        else:
+            error("configuration '{}' does not exist".format(args.config))
