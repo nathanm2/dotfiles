4386bbe20bb00d8d7d6bad8c002d07873df037c2
Nathan Miller <Nathan.Miller@hgst.com>
2016-11-14 22:32:01 -0600

    Cscope DB: Initial run support

--- cscope_db/cscope_db.py
+++ cscope_db/cscope_db.py
@@ -50,6 +50,11 @@ def get_generators(config):
 
 def list_op(config, args):
     gens = get_generators(config)
+    if args.generator:
+        gens = {k:v for k,v in gens.iteritems() if args.generator == k}
+        if not gens:
+            error("No such generator: {}".format(args.generator))
+            return
 
     print("Generators:")
     for name,path in gens.items():
@@ -76,7 +81,9 @@ if __name__ == "__main__":
     config = ConfigParser.RawConfigParser()
     config.read(configname)
 
-    # Handle the remaining arguments:
+    default_gen = config.get("config", "default")
+
+    # Create sub-parsers to process the remaining commands:
     parser = argparse.ArgumentParser(description=__doc__)
     parser.set_defaults(func=list_op)
     parser.add_argument("--config", "-c",
@@ -88,11 +95,25 @@ if __name__ == "__main__":
             title="Subcommands",
             metavar="")
 
+    # List Sub-command:
     list_parser = subparsers.add_parser('list',
             help="List generators and projects.")
-    default_gen = config.get("config", "default")
-    list_parser.add_argument("generator", nargs='?')
+    list_parser.add_argument("generator")
     list_parser.set_defaults(func=list_op)
+
+    # Init Sub-command:
+    init_parser = subparsers.add_parser('init',
+            help="Initialize a project.")
+    init_parser.add_argument("--name", "-n",
+            help="Optional name of the project")
+    init_parser.add_argument("-i", "--input",
+            help="The root of the source tree. Default: CWD",
+            default=os.getcwd())
+    init_parser.add_argument("-o", "--output",
+            help="Where to store the output '.cscope' directory. Default: CWD",
+            default=os.getcwd())
+    init_parser.add_argument("generator", nargs='?', default=default_gen)
+    init_parser.set_defaults(func=init_op)
 
     args = parser.parse_args(args)
 
