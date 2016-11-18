7aac7f05668908c93e0fa9d8be84bcd29e77521d
Nathan Miller <Nathan.Miller@wdc.com>
2016-11-18 15:20:34 -0600

    Cscope DB: Added support for 'run' subcommand

--- cscope_db/cscope_db.py
+++ cscope_db/cscope_db.py
@@ -14,9 +14,35 @@ import json
 import errno
 import datetime
 
-NAME= os.path.basename(__file__)
+
+# Module Exceptions:
+class Error(Exception):
+    """ Base class for exceptions in this module."""
+    pass
+
+class ProjectError(Error):
+    """ Project related error. """
+    def __init__(self, msg):
+        self.message = msg
+
+class ProjectNameError(ProjectError):
+    """ Exception raised when a project cannot be found by name """
+    def __init__(self, projectname):
+        msg = "No project found in database: {}".format(projectname)
+        super().__init__(msg)
+
+class GeneratorError(Error):
+    """ Generator related error. """
+    def __init__(self, generator):
+        self.message = "No generator found: {}".format(generator)
 
 # Utilities:
+
+NAME= os.path.basename(__file__)
+
+def error(msg):
+    """ Display an error message to the standard error stream. """
+    sys.stderr.write("** {}: {}\n".format(NAME, msg))
 
 def script_dir():
     """ Return the directory path containing this script."""
@@ -41,6 +67,7 @@ def create_config(configname):
     config.set('config', 'generators', generators)
     config.set('config', 'default', 'basic')
     config.set('config', 'dbfile', os.path.join(configdir, "db.json"))
+    config.set('config', 'runname', 'cs_runner.sh')
 
     ensure_dir(configdir)
     with open(configname, 'wb') as configfile:
@@ -71,9 +98,6 @@ def write_db(config, db):
         os.fsync(tmpfile.fileno())
     os.rename(tmp, dbname)
 
-def error(msg):
-    """ Display an error message to the standard error stream. """
-    sys.stderr.write("** {}: {}\n".format(NAME, msg))
 
 def get_generators(config):
     """ Get a dict of {'generator name' => 'generator path'} """
@@ -81,7 +105,7 @@ def get_generators(config):
     return {cfg: os.path.join(gendir,cfg) for cfg in
             os.listdir(gendir) if not cfg.startswith(('.','_'))}
 
-def generate_project_name(db, root):
+def make_project_name(db, root):
     """ Generate a unique project name based on the source root path. """
     (head, base) = os.path.split(root)
     suffix = ""
@@ -93,28 +117,42 @@ def generate_project_name(db, root):
         idx += 1
         suffix = str(idx)
 
-def find_project(db, cwd=os.cwd()):
-    """ Find the closest matching project."""
+def find_project_by_dir(db, dir=os.getcwd()):
+    """ Find the closest matching project by directory."""
     result_len = 0
     result = None
-    cwd_len = len(cwd)
+    dir_len = len(dir)
 
-    for projectname,project in db.items():
+    for project in db.values():
         root = project['root']
         root_len = len(root)
-        if root_len <= cwd_len and cwd.startswith(root):
+        if root_len <= dir_len and dir.startswith(root):
             if root_len > result_len:
-                result = projectname
+                result = project
                 result_len = root_len
+
+    if not result:
+        msg = "No parent project found for directory: {}".format(dir)
+        raise ProjectError(msg)
+
     return result
+
+def find_project(db, projectname=None):
+    if projectname:
+        try:
+            return db[projectname]
+        except KeyError:
+            raise ProjectNameError(projectname)
+    else:
+        return find_project_by_dir(db)
+
 
 def list_op(config, args):
     gens = get_generators(config)
     if args.generator:
         gens = {k:v for k,v in gens.iteritems() if args.generator == k}
         if not gens:
-            error("No such generator: {}".format(args.generator))
-            return 1
+            raise GeneratorError(args.generator)
 
     print("Generators:")
     for name,path in gens.items():
@@ -130,8 +168,8 @@ def list_op(config, args):
         print("    Output: {0}\n".format(entry["output"]))
     return 0
 
-def build_generator_script(genpath, root, output):
-    script = os.path.join(output, "cscope_gen.sh")
+def build_generator_script(genpath, name, root, output):
+    script = os.path.join(output, name)
 
     ensure_dir(output)
     with open(script, 'wb') as sf:
@@ -149,40 +187,41 @@ def init_op(config, args):
     if generators.has_key(generator):
         genpath = generators[generator]
     else:
-        error("No such generator: {}".format(generator))
-        return 1
+        raise GeneratorError(generator)
 
     # Cleanup any old projects with the same output directory.
     # Check for project name conflicts at the same time.
     output = os.path.normpath(args.output)
     for project in db.values():
+        projectname = project['name']
         if output == project['output']:
             rm_project_files(project)
-        elif args.name == project['name']:
-            error("Project name already in use: {}".format(args.name))
-            return 1
+            del db[projectname]
+        elif args.name == projectname:
+            msg = "Project name already in use: {}".format(projectname)
+            raise ProjectError(msg)
 
     # Build a shell script for invoking the generator:
-    script = build_generator_script(genpath, args.root, output)
+    runner = build_generator_script(genpath, args.runname, args.root, output)
 
     # Run the generator script:
-    os.system(script)
+    os.system(runner)
 
     # Create a unique project name if not provided by the caller:
-    name = args.name or generate_project_name(db, args.root)
+    name = args.name or make_project_name(db, args.root)
 
     # Update the database:
     project = {'name': name,
                'output': output,
                'generator': generator,
                'root': args.root,
+               'runner': runner,
                'updated':'{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
               }
     db[name] = project
 
     # Write the database:
     write_db(config, db)
-    return 0
 
 def rm_project_files(project):
     """
@@ -201,20 +240,23 @@ def rm_project_files(project):
 def clear_op(config, args):
     db = read_db(config)
     project = find_project(db, args.name)
-    if project:
-        # Remove the project files and remove it's entry from the database:
-        rm_project_files(project)
-        del db[project['name']]
-        write_db(config, db)
+    rm_project_files(project)
+    del db[project['name']]
+    write_db(config, db)
 
 def find_op(config, args):
     db = read_db(config)
+    project = find_project_by_dir(db)
+    print("{0} {1}".format(project['name'], project['output']))
+
+def run_op(config, args):
+    db = read_db(config)
     project = find_project(db, args.name)
-    if project:
-        print("{0}:{1}".format(project['name'], project['output']))
+    os.system(project['runner'])
+    project['updated'] = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+    write_db(config, db)
 
-if __name__ == "__main__":
-
+def main():
     # The path to the default cscope_db config file:
     default_config = os.path.join(os.path.expanduser("~"), ".cscope_db",
             "config")
@@ -249,30 +291,62 @@ if __name__ == "__main__":
     # List Sub-command:
     list_parser = subparsers.add_parser('list',
             help="List generators and projects.")
-    list_parser.add_argument("generator", nargs='?')
+    list_parser.add_argument("--generator", "-g",
+            help="Filter by generator")
+    list_parser.add_argument("names",
+            help="Project names.", nargs='*')
     list_parser.set_defaults(func=list_op)
 
     # Init Sub-command:
     init_parser = subparsers.add_parser('init',
             help="Initialize a project.")
-    init_parser.add_argument("--name", "-n",
-            help="Optional name of the project")
     init_parser.add_argument("-r", "--root",
             help="The root of the source tree. Default: CWD",
             default=os.getcwd())
     init_parser.add_argument("-o", "--output",
             help="Where to store the output. Default: CWD/.cscope",
             default=os.path.join(os.getcwd(), '.cscope'))
-    init_parser.add_argument("generator", nargs='?', default=default_gen)
+    init_parser.add_argument("-g", "--generator",
+            help="Cscope generator to use.  Default: " + default_gen,
+            default=default_gen)
+    runname = config.get("config", "runname")
+    init_parser.add_argument("-n", "--runname",
+            help="The name of runner script.  Default: " + runname,
+            default=runname)
+    init_parser.add_argument("name",
+            help="Project name.", nargs='?')
     init_parser.set_defaults(func=init_op)
 
     # Clear Sub-command:
     clear_parser = subparsers.add_parser('clear',
             help="Remove a project's cscope files.")
-    clear_parser.add_argument("name", nargs='?')
+    clear_parser.add_argument("name",
+            help="Project name.", nargs='?')
     clear_parser.set_defaults(func=clear_op)
 
+    # Find Sub-command:
+    clear_parser = subparsers.add_parser('find',
+            help="Find the closest project from the current working directory.")
+    clear_parser.set_defaults(func=find_op)
+
+    # Refresh the cscope information:
+    run_parser = subparsers.add_parser('run',
+            help="Re-run the cscope generator.")
+    run_parser.add_argument("name",
+            help="Project name.", nargs='?')
+    run_parser.set_defaults(func=run_op)
+
+    # Parse the arguments:
     args = parser.parse_args(args)
 
     # Invoke the appropriate sub-operation:
-    args.func(config, args)
+    try:
+        args.func(config, args)
+        return 0
+    except Error as e:
+        error(e.message)
+        return 1
+
+if __name__ == "__main__":
+    main()
+
