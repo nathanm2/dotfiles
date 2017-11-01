7766a1f9e8a3cada85e6de69e5e57796cbafd4e0
Nathan Miller <nathanm2@gmail.com>
2017-11-01 13:35:21 -0500

    Improved prompt color scheme

--- bashrc_common.sh
+++ bashrc_common.sh
@@ -39,4 +39,4 @@ fi
 #   $                    : Displays the dollor sign.
 #
 source ${DIR}/git-prompt.sh
-PROMPT_COMMAND='__git_ps1 "\[\033]0;\u@\h: \w " "\007\]\[\033[1;33m\]${debian_chroot:+($debian_chroot)}\[\033[1;32m\]\u@\h\[\033[0m\]:\w\$ "'
+PROMPT_COMMAND='__git_ps1 "\[\033]0;\u@\h: \w " "\a\]\[\033[1;33m\]${debian_chroot:+($debian_chroot)}\[\033[1;32m\]\u@\h\[\033[0m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "'
